952208940da90a4299e53511e371b9b728cffbf9
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-07-22 10:05:30 +0700

    setup i18n service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { I18nService } from 'nestjs-i18n';
+import { I18nTranslations } from './generated/i18n.generated';
 
 @Injectable()
 export class AppService {
+  constructor(private i18n: I18nService<I18nTranslations>) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.i18n.t('users.hello');
   }
 }
