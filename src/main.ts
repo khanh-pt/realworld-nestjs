1862634920e972522adba08ed40971624daecd31
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-07-31 14:21:33 +0700

    Use validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,28 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AllConfigType } from './config/config.type';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
+import {
+  HttpStatus,
+  UnprocessableEntityException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<AllConfigType>);
   const port = configService.getOrThrow('app.port', { infer: true });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new UnprocessableEntityException(errors);
+      },
+    }),
+  );
   app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(port);
 }
