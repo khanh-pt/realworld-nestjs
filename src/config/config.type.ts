ad1a6ab11dc9faa887707ac2b86407b89502e2a9
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-08-04 15:54:30 +0700

    Setup jwt service

--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,7 +1,9 @@
-import { AppConfig } from './app-config.type';
+import { JwtConfig } from '@/api/jwt/config/jwt-config.type';
 import { DatabaseConfig } from '@/database/config/database-config.type';
+import { AppConfig } from './app-config.type';
 
 export type AllConfigType = {
   app: AppConfig;
   database: DatabaseConfig;
+  jwt: JwtConfig;
 };
