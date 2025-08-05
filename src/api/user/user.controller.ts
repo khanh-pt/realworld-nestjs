124f94ca791f0da7d5fe3581effb5465660870ac
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-08-05 14:53:27 +0700

    Get current user

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Public } from 'src/decorators/public.decorator';
+import { AuthenticatedRequest } from 'src/types/request.type';
+import { GetUserResDto } from './dto/get-user.res.dto';
 import { LoginReqDto } from './dto/login.req.dto';
 import { LoginResDto } from './dto/login.res.dto';
 import { RegisterReqDto } from './dto/register.req.dto';
@@ -8,6 +11,8 @@ import { UserService } from './user.service';
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Public()
   @Post('users')
   async register(
     @Body() dto: RegisterReqDto,
@@ -15,8 +20,16 @@ export class UserController {
     return await this.userService.register(dto);
   }
 
+  @Public()
   @Post('users/login')
   async login(@Body() dto: LoginReqDto): Promise<{ user: LoginResDto }> {
     return await this.userService.login(dto);
+  }
+
+  @Get('user')
+  async getUser(
+    @Request() req: AuthenticatedRequest,
+  ): Promise<{ user: GetUserResDto }> {
+    return await this.userService.getUser(req.user);
   }
 }
