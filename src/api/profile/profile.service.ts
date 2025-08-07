6df6866cca06c69cd4fe640be658666af64e78be
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-08-07 15:17:09 +0700

    Follow user

--- src/api/profile/profile.service.ts
+++ src/api/profile/profile.service.ts
@@ -1,20 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm/repository/Repository';
+import { FollowEntity } from '../follow/entities/follow.entity';
 import { UserEntity } from '../user/entities/user.entity';
-import { GetProfileResDto } from './dto/profile.dto';
+import { FollowUserResDto } from './dto/follow-user.res.dto';
+import { GetProfileResDto } from './dto/get-profile.res.dto';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async getProfile(username: string): Promise<{ profile: GetProfileResDto }> {
     const user = await this.userRepository.findOne({ where: { username } });
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
     return {
@@ -23,6 +31,42 @@ export class ProfileService {
         bio: user.bio,
         image: user.image,
         following: false,
+      },
+    };
+  }
+
+  async followUser(
+    followerId: number,
+    username: string,
+  ): Promise<{ profile: FollowUserResDto }> {
+    const followingUser = await this.userRepository.findOne({
+      where: { username },
+    });
+
+    if (!followingUser) {
+      throw new NotFoundException('User not found');
+    }
+    if (followerId === followingUser.id) {
+      throw new BadRequestException('You cannot follow yourself');
+    }
+
+    const isFollowing = await this.followRepository.exists({
+      where: { followerId, followingId: followingUser.id },
+    });
+
+    if (!isFollowing) {
+      await this.followRepository.save({
+        followerId,
+        followingId: followingUser.id,
+      });
+    }
+
+    return {
+      profile: {
+        username: followingUser.username,
+        bio: followingUser.bio,
+        image: followingUser.image,
+        following: true,
       },
     };
   }
